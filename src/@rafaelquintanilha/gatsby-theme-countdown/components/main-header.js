95ba2c8729107a8416dbc9bf6c6dcffa6d1d358c
Sohini Mukherjee <sohini.mukherjee@pncl.tech>
2022-12-24 15:21:50 -0500

    seo

--- src/@rafaelquintanilha/gatsby-theme-countdown/components/main-header.js
+++ src/@rafaelquintanilha/gatsby-theme-countdown/components/main-header.js
@@ -7,7 +7,14 @@ import favicon from "../../../images/favicon.png";
 const MainHeader = ({ text }) => (
   <div style={{height: "100%"}} className="main-header">
       <Helmet>
+          <title>Whatever</title>
           <meta property='og:image' content={ favicon } />
+          <meta property='og:locale' content='en_US' />
+          <meta property='og:type' content='website' />
+          <meta property='og:title' content='South Asian Queer Collective' />
+          <meta property='og:description' content="SAQC amplifies queer South Asian voices often silenced by white, cis-heteronormative stories within the mainstream." />
+          <meta property='og:url' content='https://theysiqueers.ca' />
+          <meta property='og:updated_time' content='2022-12-24' />
       </Helmet>
     <Image
         className="main-header--logo"
