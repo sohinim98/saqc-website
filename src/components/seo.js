df9c9456d5d80e1fa52367d4ba48e7b23437ba58
Sohini Mukherjee <sohini.mukherjee@pncl.tech>
2022-12-24 15:01:45 -0500

    fixes

--- src/components/seo.js
+++ src/components/seo.js
@@ -30,17 +30,17 @@ function Seo({ description, title, children }) {
 
   return (
     <>
-      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
-      <meta name="description" content={metaDescription} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={metaDescription} />
-      <meta property="og:type" content="website" />
+        <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+        <meta name="description" content={metaDescription} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={metaDescription} />
+        <meta property="og:type" content="website" />
         <meta property="og:image" content={favicon} />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={metaDescription} />
-      {children}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={metaDescription} />
+        {children}
     </>
   )
 }
