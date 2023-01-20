419c0703a05996487b840251ab744df44e8363a5
Sohini Mukherjee <sohini.mukherjee@pncl.tech>
2023-01-20 01:12:55 -0500

    removed transition for now

--- src/components/image-swapper.js
+++ src/components/image-swapper.js
@@ -7,8 +7,8 @@ const ImageSwapper = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const images = ['gr1.jpg', 'asb1.jpg', 'whabh6.jpg', 'btgb1.jpg', 'gr2.jpg', 'asb14.jpg',
         'whabh13.jpg', 'btgb8.jpg', 'gr3.jpg', 'asb2.jpg', 'whabh22.jpg', 'btgb15.jpg'];
-    const duration = 8;
-    const transition = 2;
+    // const duration = 8;
+    // const transition = 2;
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -23,7 +23,7 @@ const ImageSwapper = () => {
         return () => clearInterval(intervalId);
     }, [currentIndex,images.length])
 
-    return <div style={{animationDelay: `${(duration + transition) * currentIndex}s`}}>
+    return <div>
         <Image
             className="home-cover"
             src={images[currentIndex]}
